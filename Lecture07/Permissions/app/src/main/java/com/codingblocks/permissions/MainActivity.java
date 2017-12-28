04aa4a24b7b122db9a98f0f56e10ef9a6156461a
Arnav Gupta <arnav@codingblocks.com>
2017-12-28 11:51:21 +0530

    dialog to ask for permission
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture07/Permissions/app/src/main/java/com/codingblocks/permissions/MainActivity.java
+++ Lecture07/Permissions/app/src/main/java/com/codingblocks/permissions/MainActivity.java
@@ -1,12 +1,21 @@
 package com.codingblocks.permissions;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -36,10 +45,18 @@ public class MainActivity extends AppCompatActivity {
         btnWriteFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
+                int perm = ContextCompat.checkSelfPermission(
+                        MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                if (perm == PackageManager.PERMISSION_GRANTED) {
                     writeFile();
-                } catch (IOException e) {
-                    Log.e(TAG, "Could not write file", e);
+                } else {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            44
+                    );
                 }
             }
         });
@@ -47,34 +64,95 @@ public class MainActivity extends AppCompatActivity {
         btnReadFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
+                int perm = ContextCompat.checkSelfPermission(
+                        MainActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                if (perm == PackageManager.PERMISSION_GRANTED) {
                     tvFileData.setText(readFile());
-                } catch (IOException e) {
-                    Log.e(TAG, "Could not read file", e);
+                } else {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                            45
+                    );
                 }
             }
         });
     }
 
-    void writeFile () throws IOException {
-        File sdCard = Environment.getExternalStorageDirectory();
-        File helloTxt = new File(sdCard, "hello.txt");
-        FileOutputStream foutStream = new FileOutputStream(helloTxt, true);
-        foutStream.write("Hello World".getBytes());
+    void writeFile () {
+        try {
+            File sdCard = Environment.getExternalStorageDirectory();
+            File helloTxt = new File(sdCard, "hello.txt");
+            FileOutputStream foutStream = new FileOutputStream(helloTxt, true);
+            foutStream.write("Hello World".getBytes());
+        } catch (IOException ioe) {
+            Log.e(TAG, "writeFile: ", ioe);
+        }
     }
 
-    String readFile() throws IOException {
-        File sdCard = Environment.getExternalStorageDirectory();
-        File helloTxt = new File(sdCard, "hello.txt");
-        FileInputStream finStream = new FileInputStream(helloTxt);
-        BufferedReader br = new BufferedReader(new InputStreamReader(finStream));
-        StringBuilder sb = new StringBuilder("");
-        String buf = br.readLine();
-        while (buf != null) {
-            sb.append(buf);
-            sb.append("\n");
-            buf = br.readLine();
+    String readFile() {
+        try {
+            File sdCard = Environment.getExternalStorageDirectory();
+            File helloTxt = new File(sdCard, "hello.txt");
+            FileInputStream finStream = new FileInputStream(helloTxt);
+            BufferedReader br = new BufferedReader(new InputStreamReader(finStream));
+            StringBuilder sb = new StringBuilder("");
+            String buf = br.readLine();
+            while (buf != null) {
+                sb.append(buf);
+                sb.append("\n");
+                buf = br.readLine();
+            }
+            return sb.toString();
+        } catch (IOException ioe) {
+            Log.e(TAG, "readFile: ", ioe);
+            return "";
         }
-        return sb.toString();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == 44) { //write request
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                writeFile();
+            } else {
+                Toast.makeText(this, "Writing file required this permission", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        if (requestCode == 45) { //read request
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                tvFileData.setText(readFile());
+            } else {
+                Toast.makeText(this, "Reading file required this permission", Toast.LENGTH_SHORT).show();
+                new AlertDialog.Builder(this)
+                        .setMessage("We need this permission to read the file.\n" +
+                                "Please allow this permission")
+                        .setPositiveButton("GIVE PERMISSION", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(
+                                        MainActivity.this,
+                                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                                        45
+                                );
+                            }
+                        })
+                        .setNegativeButton("NO THANKS", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Sigh! I tried", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        }
     }
 }
