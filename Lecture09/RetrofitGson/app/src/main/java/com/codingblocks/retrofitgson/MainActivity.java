99ebe84a7e0e9bfe8bd008dfed5694afd413417f
Arnav Gupta <arnav@codingblocks.com>
2018-01-06 12:23:01 +0530

    upload assignment
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture09/RetrofitGson/app/src/main/java/com/codingblocks/retrofitgson/MainActivity.java
+++ Lecture09/RetrofitGson/app/src/main/java/com/codingblocks/retrofitgson/MainActivity.java
@@ -2,11 +2,14 @@ package com.codingblocks.retrofitgson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.codingblocks.retrofitgson.models.Post;
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,6 +18,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "POST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +33,17 @@ public class MainActivity extends AppCompatActivity {
 
         JsonPlaceholder api = r.create(JsonPlaceholder.class);
 
-        api.getPostById(1).enqueue(new Callback<Post>() {
-            @Override
-            public void onResponse(Call<Post> call, Response<Post> response) {
-                Post p = response.body();
 
+        api.getPosts().enqueue(new Callback<ArrayList<Post>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Post>> call, Response<ArrayList<Post>> response) {
+                for (Post p : response.body()) {
+                    Log.d(TAG, "onResponse: " + p.getTitle());
+                }
             }
 
             @Override
-            public void onFailure(Call<Post> call, Throwable t) {
+            public void onFailure(Call<ArrayList<Post>> call, Throwable t) {
 
             }
         });
